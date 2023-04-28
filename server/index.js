5a92a13261d1db85590d238fb7533e0a597d9bd9
Luka <lukapodra1@gmail.com>
2023-04-28 10:36:14 +0200

    accept get rquest to stay awake

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const { PORT } = process.env
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.status(200).json('App recieved get request')
+})
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 })
